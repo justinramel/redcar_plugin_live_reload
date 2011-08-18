635f66c05a72c6d15224b15b65da9d0017ab4115
Justin Ramel <justin.ramel@gmail.com>
2011-08-19 00:17:42 +0100

    Updating README with blog post. Need to make this into a proper  README.

--- lib/live_reload/refresh_browser.rb
+++ lib/live_reload/refresh_browser.rb
@@ -6,25 +6,36 @@ module Redcar
       end
 
       def description
-        "reload browser"
+        "refresh browser"
       end
 
       def execute
-        keyword = 'Starcraft'
-        %x{osascript<<-ENDGAME
-tell application "Google Chrome"
-set windowList to every window
-repeat with aWindow in windowList
-  set tabList to every tab of aWindow
-  repeat with atab in tabList
-    if (URL of atab contains "#{keyword}") then
-      tell atab to reload
-    end if
-  end repeat
-end repeat
-end tell
+        keyword = storage['keyword']
+        run_chrome_script(keyword)
+      end
+
+      private
+      def storage
+        @storage ||= Plugin::Storage.new('live_reload_plugin')
+        @storage.set_default('keyword', '')
+        @storage
+      end
+
+      def run_chrome_script(keyword)
+        %x{osascript<<ENDGAME
+          tell application "Google Chrome"
+            set windowList to every window
+            repeat with aWindow in windowList
+              set tabList to every tab of aWindow
+              repeat with atab in tabList
+                if (URL of atab contains "#{keyword}") then
+                  tell atab to reload
+                end if
+              end repeat
+            end repeat
+          end tell
 ENDGAME
-        }
+}
       end
     end
   end
